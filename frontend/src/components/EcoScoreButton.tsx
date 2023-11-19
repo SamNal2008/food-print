b04c53a47218c0795f39d20924518df1917d8ac6
samy <samy.nalbandian@epita.fr>
2023-11-19 17:14:12 +0100

    feat: display result

--- frontend/src/components/EcoScoreButton.tsx
+++ frontend/src/components/EcoScoreButton.tsx
@@ -6,6 +6,19 @@ interface Props {
   img?: string
 }
 
+const Result = ({ score }: { score?: MyFoodPrint }) => {
+  if (!score) return null;
+  return (
+    <div>
+      Hello, your meal's Eco Score is: {score.overallScore}
+      <br/>
+      Here is the list of your ingredients meal : {score.ingredients.map((ingredient, index) => {
+        return <li key={index}>{ingredient} : {score.ingredientScores[index]}</li>
+      })}
+    </div>
+  )
+}
+
 const EcoScoreButton = ({ img }: Props) => {
 
   const [score, setScore] = useState<MyFoodPrint>();
@@ -16,7 +29,8 @@ const EcoScoreButton = ({ img }: Props) => {
       return;
     }
     const logMealResponse = await getLogMeal(img);
-    const score = await computeMealScore(logMealResponse);
+    const tmpScore = await computeMealScore(logMealResponse);
+    setScore(tmpScore);
     console.log('Score: ', score);
   }
 
@@ -25,6 +39,7 @@ const EcoScoreButton = ({ img }: Props) => {
       <button onClick={ () => computeEcoScore(img)}>
         Click here to get your meal's Eco Score!
       </button>
+      <Result score={score} />
     </div>
   )
 }
